632fd52c2a959c7b60ad767b3efb8ad6c12a16f0
ivanshikenya <ivanshikenya@yandex.ru>
2020-10-21 15:39:09 +0700

    Install redux-saga

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
 import ListView from "./features/list/ListView";
 
@@ -8,10 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-{/*
-        <Counter />
-*/}
-        <ListView />
+          <ListView />
       </header>
     </div>
   );
